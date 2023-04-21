2dd88d33e4b822af9e3a261b5c2048713dd84283
Aribunfi <arianna@pabis.de>
2023-04-21 18:04:57 +0200

    aggiunto nuovo seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ProjectSeeder::class,
             UserSeeder::class,
             CategorySeeder::class,
+            TechnologySeeder::class,
           ]);
     }
 }
