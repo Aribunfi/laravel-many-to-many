052dbe398f3631450a3cf184bf3e8a450a422f83
Aribunfi <arianna@pabis.de>
2023-04-21 17:40:58 +0200

    colonne tabella technlogies

--- database/migrations/2023_04_21_153252_create_technologies_table.php
+++ database/migrations/2023_04_21_153252_create_technologies_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('HTML');
+            $table->string('Vue js');
+            $table->string('Javascript');
+            $table->string('Adobe suite');
         });
     }
 
