badae3ed1c41ffb5a0bf6e25b6ecb55f85ee969a
Aribunfi <arianna@pabis.de>
2023-04-21 18:03:50 +0200

    importato faker

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +13,17 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $labels = ["HTML", "Vue js", "Javascript", "Adobe suite"];
+    
+        
+        
+        foreach($labels as $label){
+            $technology = new Technology();
+            $technology->label = $label;
+
+            $technology->save();
+        }
     }
 }
