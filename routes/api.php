e832a299677d601d10401fe8b22f37e335a43a34
Aribunfi <arianna@pabis.de>
2023-04-26 16:25:20 +0200

    cancellato auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-
-});
-
 return response()->json([
     'tile'=>"Biblioteca",
     'year'=>"2022",
