580c4b45fd4859ea139b8d4c0e37b9e858465078
Aribunfi <arianna@pabis.de>
2023-04-28 17:33:07 +0200

    aggiornamento project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -16,13 +16,21 @@ class ProjectSeeder extends Seeder  {
      */
     public function run(Faker $faker)
     {
+        $categories = Category::all()->pluck('id')->toArray();
+
         for($i = 0; $i < 50; $i++) {
+
+            $category_id = (random_int(0,1)===1) ? $faker->randomElement($categories) : null;
+
             $project = new Project;
+            $project->category_id = $category_id;
             $project->title = $faker->firstNameFemale();
+            $project->slug = Str::of($project->title)->slug('-');
             $project->year = $faker->unique()->numberBetween(2009, 2023);
             $project->kind = $faker->randomElement(['graphic', 'web', 'writing']);
             $project->time = $faker->unique()->numberBetween(1, 6);
             $project->description = $faker->paragraph(12);
+            $project->is_published = random_int(0,1);
             $project->save();
     }
 }}
